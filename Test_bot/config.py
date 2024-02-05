ceaa52512868783a9eb274ae738f514c19427928
Alfred Bagdasarov <alfred.bahdasarov@gmail.com>
2024-02-05 20:20:17 +0300

    added Test_bot

---
+++ Test_bot/config.py
@@ -0,0 +1,10 @@
+DB_PATH = "database.db"
+BOT_TOKEN = "<6595177066:AAFdW6lCvbTZ8wZ4Ex9s0OS41PIQt8Ns-uc>"
+
+DEFAULT_COMMANDS = (
+    ("newtask", "Создать задачу"),
+    ("tasks", "Последние 10 задач"),
+    ("today", "Задачи на сегодня"),
+)
+
+DATE_FORMAT = "%d.%m.%Y"
